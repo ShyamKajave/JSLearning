86528be0ae5cc879ef6ad56f4f9f0841c884a58b
Shyam Kajave <shyamkajave@gmail.com>
2023-07-21 07:10:54 +0530

    swaps
    swap assignment

---
+++ 03_swap.js
@@ -0,0 +1,26 @@
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log('Before swap==>', 'num1==>', num1 , 'num2==>', num2 ,'num==>', num3);
+
+var temp = num1;
+ num1=num2;
+ num2=num3;
+ num3=temp;
+
+ console.log('Before swap==>', 'num1==>', num1 , 'num2==>', num2 ,'num==>', num3);
+
+ console.log('---------------------------------------------------------------------------------');
+
+ var name1 = "cutie";
+ var name2 = "sweety";
+
+
+ console.log('Before swap==>', 'name1==>',name1, 'name2==>' ,name2);
+
+ var temp = name1;
+
+ name1=name2;
+ name2=temp;
+
+ console.log('Before swap==>', 'name1==>',name1, 'name2==>' ,name2);
---
+++ helloWorld.js
@@ -0,0 +1,20 @@
+// var salary = 54000;
+// console.log('salary==', salary, 'type of==',typeof salary);
+// console.log('=======================================');
+// var salary = null;
+// console.log(typeof salary);
+// salary = 40000.50; // 
+// var typeOfSalary = typeof salary;
+// console.log('salary=>', salary, 'typeof salary =>', typeOfSalary);
+// salary = "Forty Thousand";
+// var salaryTypeOf = typeof salary;
+// console.log('salary=>', salary, 'typeof=>', salaryTypeOf);
+
+var a=100;
+var b=200;
+
+console.log('before swapping a=', a, 'b=',b);
+var temp=a;
+a=b;
+b=temp;
+console.log('after swapping a=', a, 'b=',b);
