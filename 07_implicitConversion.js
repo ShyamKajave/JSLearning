8be2625fd67ab308ba37b48ed26db18292164bf9
Shyam Kajave <shyamkajave@gmail.com>
2023-07-27 19:58:37 +0530

    implicit conversion ass
    implicit conversion ass

---
+++ 07_implicitConversion.js
@@ -0,0 +1,66 @@
+console.log(`**************************************************`);
+
+var result = '3' + 2;
+console.log(`${result}`);
+console.log(`We got this result because it is internall number converted to string`);
+
+console.log(`**************************************************`);
+
+var result = '3' + true;
+console.log(`${result}`);
+console.log(`This why because boolen value converted to string`);
+
+console.log(`**************************************************`);
+
+var result = '3' + undefined;
+console.log(`${result}`);
+console.log(`Undefined value converted to string`);
+
+console.log(`**************************************************`);
+
+var result = '3' + null;
+console.log(`${result}`);
+console.log(`Null value converted to string`);
+
+console.log(`**************************************************`);
+
+var result = '4' - true;
+console.log(`${result}`);
+console.log(`Boolen value converted to 1`);
+
+console.log(`**************************************************`);
+
+var result = 4+true;
+console.log(`${result}`);
+console.log(`True value internally converted to number`);
+
+console.log(`**************************************************`);
+
+var result = 4+false;
+console.log(`${result}`);
+console.log(`Boolen Value is false so their no chage`);
+
+console.log(`**************************************************`);
+
+var result = '4' - '2';
+console.log(`${result}`);
+console.log(`String Value converted to number internally`);
+
+console.log(`**************************************************`);
+
+var result = '4' - 2;
+console.log(`${result}`);
+console.log(`String value converted to number`);
+
+console.log(`**************************************************`);
+
+var result = '4' *2;
+console.log(`${result}`);
+console.log(`String value converted to number internally`);
+
+console.log(`**************************************************`);
+
+var result = '4' / 2;
+console.log(`${result}`);
+console.log(`String value converted to number internally`);
+
